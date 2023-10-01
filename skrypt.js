548f1dce7ac691464abe3654f9addd97de0c3998
avekpl <91049475+avekpl@users.noreply.github.com>
2023-10-01 16:29:35 +0200

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,29 @@
+function malinowski_zadanie1()
+{
+    window.open("malinowski_zadanie1.html","malinowski_zadanie1","toolbar=no,directories=no,menubar=no,height=700,width=700,top=700,left=1000");
+}
+function malinowski_zadanie1_kod()
+{
+    window.open("malinowski_zadanie1_kod.html","malinowski_zadanie1_kod","toolbar=no,directories=no,menubar=no,height=700,width=700,top=700,left=1000");
+}
+function malinowski_zadanie2()
+{
+    window.open("malinowski_zadanie2.html","malinowski_zadanie2","toolbar=no,directories=no,menubar=no,height=350,width=700,top=1000,left=0");
+}
+function malinowski_zadanie2_kod()
+{
+    window.open("malinowski_zadanie2_kod.html","malinowski_zadanie2_kod","toolbar=no,directories=no,menubar=no,height=700,width=700,top=700,left=1000");
+}
+function malinowski_zadanie3()
+{
+    window.open("malinowski_zadanie3.html","malinowski_zadanie3","toolbar=no,directories=no,menubar=no,height=700,width=700,top=700,left=1000");
+}
+function malinowski_zadanie4()
+{
+    window.open("malinowski_zadanie4.html","malinowski_zadanie4","toolbar=no,directories=no,menubar=no,height=700,width=700,top=700,left=1000");
+}
+function malinowski_zadanie5()
+{
+    window.open("malinowski_zadanie5.html","malinowski_zadanie5","toolbar=no,directories=no,menubar=no,height=700,width=700,top=700,left=1000");
+}
+
